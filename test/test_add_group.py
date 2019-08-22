e6aae957b1c152e62668bb6afb8e26082539b622
Elena Zabelina <zabelina92@mail.ru>
2019-08-23 00:04:49 +0300

    Выделен класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 @pytest.fixture
 def app(request):
@@ -9,15 +9,13 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="fgh", header="123", footer='wer'))
-    app.logout()
-
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=''))
-    app.logout()
+    app.session.logout()
 
