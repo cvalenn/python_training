465281aade460586cab562323e924f434053fb72
Elena Zabelina <zabelina92@mail.ru>
2019-08-23 18:11:17 +0300

    Тест на добавление контакта. Выделена фикстура и осуществлен переход на py.test

--- fixture/session.py
+++ fixture/session.py
@@ -13,8 +13,7 @@ class SessionHelper:
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        wd.find_element_by_xpath("//input[@value]").click()
 
     def logout(self):
         wd = self.app.wd
