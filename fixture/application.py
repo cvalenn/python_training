da5b3c53aa68869c487a5edf6085405fdb708400
Elena Zabelina <zabelina92@mail.ru>
2019-08-23 21:21:42 +0300

    Добавлена общая фикстура для всех тестов conftest.py

--- fixture/application.py
+++ fixture/application.py
@@ -3,11 +3,12 @@ from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
+
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(60)
+        self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
