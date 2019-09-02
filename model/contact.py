3fc5e96f422f4add34ec430904eb2eabc58a2f41
Elena Zabelina <zabelina92@mail.ru>
2019-09-03 02:18:23 +0300

    Добавлена проверка на равенство списков в тестах на удаление групп/контактов

--- model/contact.py
+++ model/contact.py
@@ -7,3 +7,9 @@ class Contact:
         self.nickname = nickname
         self.title = title
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname and self.lastname == other.lastname
