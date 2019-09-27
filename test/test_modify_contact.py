4c672dc2a0955c3cb643c87ceb58198089374df2
Elena Zabelina <zabelina92@mail.ru>
2019-09-27 19:05:16 +0300

    реализованы отключаемые проверки пользовательского интерфейса

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 
 
-def test_modify_contact(app, db):
+def test_modify_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="test", lastname="1234"))
     old_contacts = db.get_contact_list()
@@ -12,6 +12,8 @@ def test_modify_contact(app, db):
     new_contacts = db.get_contact_list()
     old_contacts[old_contacts.index(random_contact)] = contact
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
 
 #def test_modify_contact_middlename(app):
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 
 
-def test_modify_group_name(app, db):
+def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -12,6 +12,8 @@ def test_modify_group_name(app, db):
     new_groups = db.get_group_list()
     old_groups[old_groups.index(random_group)] = group
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 
 #def test_modify_group_header(app):
